db60c37b04f8a50e44b990399a163efc4c19cfec
asonas <hzw1258@gmail.com>
2014-04-04 16:08:05 +0900

    Remove Support class (no longer used)

--- lib/idobata/support.rb
+++
@@ -1,9 +0,0 @@
-module Idobata
-  class Support
-    def self.deploy_notification(user_name: "anonymous", application_name: "", revision_url: "", revision: "")
-      source = "[DEPLOY] #{user_name} deployed a new version of #{application_name}"
-      source += " (deployed revision: <a href=#{revision_url} target='_blank'>#{revision[0..8]}</a>)" unless revision.empty?
-      Idobata.client.post(format: :html, source: source)
-    end
-  end
-end
